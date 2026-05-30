104a5a97ade93b1c4caed6e0f8953e8c2ba38f34
Thiago <thiagoth.dantas08@gmail.com>
2026-05-30 13:22:45 -0300

    feat: cria layout base, dashboard, criação e visualização de trabalhos

--- models.py
+++ models.py
@@ -1,34 +1,39 @@
 from database import db
+import uuid
+from datetime import datetime
+
 
 class Usuario(db.Model):
     __tablename__ = "usuarios"
 
-    id = db.Column(db.String(36), primary_key=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
 
     nome = db.Column(db.String(255), nullable=False)
-
     email = db.Column(db.String(255), nullable=False, unique=True)
-
     senha = db.Column(db.Text, nullable=False)
 
     tipo_usuario = db.Column(db.String(50), nullable=False)
-
     matricula = db.Column(db.String(50))
 
     ativo = db.Column(db.Boolean, default=True)
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow)
 
-    criado_em = db.Column(db.DateTime)
 
 class Trabalho(db.Model):
     __tablename__ = "trabalhos"
 
-    id = db.Column(db.String(36), primary_key=True)
-    titulo = db.Column(db.String(255))
-    resumo = db.Column(db.Text)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+
+    titulo = db.Column(db.String(255), nullable=False)
+    resumo = db.Column(db.Text, nullable=False)
     palavras_chave = db.Column(db.Text)
+
     arquivo_url = db.Column(db.Text)
-    status = db.Column(db.String(50))
+
+    status = db.Column(db.String(50), default="pendente")
+
     aluno_id = db.Column(db.String(36))
     avaliador_id = db.Column(db.String(36))
-    criado_em = db.Column(db.DateTime)
+
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow)
     prazo_envio = db.Column(db.DateTime)
--- routes.py
+++ routes.py
@@ -1,8 +1,39 @@
-from database import app
-from flask import render_template
+from flask import render_template, redirect, request
+from database import app, db
 from models import Trabalho
+import uuid
+
+
+@app.route("/")
+def dashboard():
+    trabalhos = Trabalho.query.all()
+    return render_template("dashboard.html", trabalhos=trabalhos)
+
 
 @app.route("/trabalho/<id>")
 def visualizar_trabalho(id):
     trabalho = Trabalho.query.get(id)
     return render_template("trabalho.html", trabalho=trabalho)
+
+
+@app.route("/criar_trabalho", methods=["GET", "POST"])
+def criar_trabalho():
+    if request.method == "POST":
+
+        titulo = request.form["titulo"]
+        resumo = request.form["resumo"]
+
+        novo = Trabalho(
+            id=str(uuid.uuid4()),
+            titulo=titulo,
+            resumo=resumo,
+            status="Enviado",
+            aluno_id="11111111-1111-1111-1111-111111111111"
+        )
+
+        db.session.add(novo)
+        db.session.commit()
+
+        return redirect("/")
+
+    return render_template("criar_trabalho.html")
